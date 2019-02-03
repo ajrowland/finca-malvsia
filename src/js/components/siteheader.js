ce597d6561162372ed41bfb7aca07850044f914f
Andrew Rowland <a.rowland@amaze.com>
2019-02-03 19:20:16 +0000

    Updated navigation.

--- src/js/components/siteheader.js
+++ src/js/components/siteheader.js
@@ -2,21 +2,16 @@ class SiteHeader {
   constructor(elements) {
     elements.forEach(function(el) {
       const cacheEl = {
-        toggle: el.querySelectorAll('[data-navigation-toggle],[data-subnavigation-toggle]')
+        toggle: el.querySelectorAll('[data-navigation-toggle]')
       };
 
-      const activeClass = 'is--active';
       const navActiveClass = 'is--nav-active';
 
       cacheEl.toggle.forEach(function(el) {
         el.addEventListener('click', (ev) => {
           ev.preventDefault();
 
-          if (el.getAttribute('data-navigation-toggle') !== null) {
-            document.body.classList.contains(navActiveClass) ? document.body.classList.remove(navActiveClass) : document.body.classList.add(navActiveClass);
-          }
-
-          el.parentNode.classList.contains(activeClass) ? el.parentNode.classList.remove(activeClass) : el.parentNode.classList.add(activeClass);
+          document.body.classList.contains(navActiveClass) ? document.body.classList.remove(navActiveClass) : document.body.classList.add(navActiveClass);
         }, false);
       });
     });
