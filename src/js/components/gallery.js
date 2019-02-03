cf3a3d8a36d57727a754192a5d3abd4a87c862e5
Andrew Rowland <a.rowland@amaze.com>
2019-02-03 18:06:56 +0000

    Finished gallery.

--- src/js/components/gallery.js
+++ src/js/components/gallery.js
@@ -26,27 +26,41 @@ class Gallery {
           cacheEl.indicatorContainer.appendChild(indicatorItem);
         });
 
+        const methods = {
+          changeImage: function(item) {
+            currentItem = item;
+
+            if (currentItem < 0) {
+              currentItem = items.length - 1;
+            } else if (currentItem > items.length -1) {
+              currentItem = 0;
+            }
+  
+            items.forEach(function(item) {
+              item.classList.remove(activeClass);
+            });
+  
+            indicatorItems.forEach(function(item) {
+              item.classList.remove(activeClass);
+            });
+  
+            items[currentItem].classList.add(activeClass);
+            indicatorItems[currentItem].classList.add(activeClass);
+          }
+        };
+
         const indicatorItems = Array.from(cacheEl.indicatorContainer.children);
 
+        const clickInterval = setInterval(function() {
+          methods.changeImage(currentItem + 1);
+        }, 5000);
+
         el.addEventListener('click', (ev) => {
+          clearInterval(clickInterval);
+
           currentItem += (ev.clientX - ev.target.getBoundingClientRect().left) > el.offsetWidth / 2 ? 1 : -1;
 
-          if (currentItem < 0) {
-            currentItem = items.length - 1;
-          } else if (currentItem > items.length -1) {
-            currentItem = 0;
-          }
-
-          items.forEach(function(item) {
-            item.classList.remove(activeClass);
-          });
-
-          indicatorItems.forEach(function(item) {
-            item.classList.remove(activeClass);
-          });
-
-          items[currentItem].classList.add(activeClass);
-          indicatorItems[currentItem].classList.add(activeClass);
+          methods.changeImage(currentItem);
         }, false);
       });
     });
